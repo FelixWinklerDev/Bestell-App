485bed0e5a1b8046471aaa54f4c99b3961e1ed0b
Felix Winkler <Felix-Winkler96@web.de>
2026-03-16 13:01:49 +0100

    Added Database

--- scripts/db.js
+++ scripts/db.js
@@ -1,7 +1,84 @@
 let products = [
   {
-    name: 'Veggie Mushroom Black Burger',
-    price: 14.99,
-    description: 'Mixed green salad, Tomatoes, Edamame, Mushrooms',
+    category: 'Burger',
+    items: [
+      {
+        name: 'vegetarischer Camembert-Burger',
+        price: 10.99,
+        description:
+          'Blattsalat, rote Zwiebeln, gebackenen Camembert, Preiselbeermarmelade',
+      },
+      {
+        name: 'Veganer Burger mit Roter Beete und Tofu',
+        price: 12.99,
+        description:
+          'rote Beete, Kidneybohnen, Gurken, Sojageschnetzeltes, Tomaten, hausgemachte Vinigrette',
+      },
+      {
+        name: 'Big Beef Burger',
+        price: 15.99,
+        description:
+          'Rindfleisch, Bacon, Gewürzgurken, Smoked cheese, Ketchup, BBQ Soße',
+      },
+      {
+        name: 'Chicken Burger',
+        price: 13.99,
+        description:
+          'Hähnchenbrustfilet, Knusper Panade, Feldsalat, Smocked Mayo, Tomate, getrocknete Tomate',
+      },
+    ],
+    category: 'Pizza 30cm Ø',
+    items: [
+      {
+        name: 'Salami',
+        price: 10.99,
+        description: 'Salami, Tomatensoße, Gouda',
+      },
+      {
+        name: 'Mexico',
+        price: 13.99,
+        description:
+          'Jalapeños, Hackfleisch, Kidneybohnen, Chilli, Mais, Frühlingszwiebeln, Gouda, Tomatensoße',
+      },
+      {
+        name: 'Alaska',
+        price: 15.99,
+        description:
+          'Räucherlachs, rote Zwiebeln, Knoblauch-Dill-Creme, Kaviar, Gouda',
+      },
+      {
+        name: 'Vegan',
+        price: 14.99,
+        description:
+          'Süßkartoffelteig, Kichererbsen, Spinat, getrocknete Tomaten, Veganen Käse, Tomatesoße',
+      },
+    ],
+    category: 'Salat',
+    items: [
+      {
+        name: 'Klassischer Salat',
+        price: 9.99,
+        description:
+          'gemischter Blattsalat, Paprika, Tomaten, Gurken, Granatapfelkerne, hausgemachter Vinigrette, Croûtons',
+      },
+      {
+        name: 'Ceasar Salat',
+        price: 12.99,
+        description:
+          'Hähnchenbrustfilet, Romanasalat, Original Ceasar Dressing, Croûtons',
+      },
+      {
+        name: 'Grieschicher Salat',
+        price: 8.99,
+        description:
+          'gemischter Blattsalat, Feta, schwarze Oliven, rote Zwiebeln, Cherrytoamaten, Gurke',
+      },
+      {
+        name: 'Soja Bowl',
+        price: 11.99,
+        description:
+          'Sojageschnetzeltes, Kichererbsen, Feldsalat, Couscous, getrocknete Tomate, Veganes Ranch Dressing',
+      },
+    ],
   },
 ];
